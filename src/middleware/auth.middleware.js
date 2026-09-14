e4ce1a39a134a2aafd619fc452cc22ba15d2fa53
Wasim Ansari <oasimansari0786@gmail.com>
2026-09-14 16:08:39 +0545

    created auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,29 +1,26 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.model.js";
 
-import jwt from 'jsonwebtoken'
-import userModel from '../models/user.model.js'
+async function authMiddleware(req, res, next) {
+  const token = req.cookies.token || req.header.authorization?.split(" ")[1];
 
- async function authMiddleware(req, res, next){
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized access, token is missing" });
+  }
 
-    const token = req.cookies.token || req.header.authorization?.split(" ")[1]
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if(!token){
-        return res.status(401).json({message: "Unauthorized access, token is missing"})
-    }
+    const user = await userModel.findById(decoded.userId);
 
-   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-   console.log(decoded.userId)
-    const user = await userModel.findById(decoded.userId)
-    console.log(user)
+    req.user = user;
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+}
 
-    req.user = user
-     return next()
-    
-   } catch (error) {
-    console.error(error)
-      return res.status(401).json({message: "Unauthorized access"})
-   }
-      
- }
-
- export default {authMiddleware}
+export default { authMiddleware };
